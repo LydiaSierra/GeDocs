2fb8d6d47a220e92e1e79693ddfc2fa557e70b6a
Lawliet26 <luisacoy2005@gmail.com>
2025-11-08 14:55:56 -0500

    login feature fixed
    Se ajusto el modelo de user agregandole la columna para recordar el token de acceso al inicio de sesión

--- app/Models/PQR.php
+++ app/Models/PQR.php
@@ -3,8 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\TypeEnum;
 
 class PQR extends Model
 {
-    //
+    protected $table = 'p_q_r_s';
+    protected $fillable = [
+        'document_type',
+        'document',
+        'name',
+        'address',
+        'email',
+        'phone',
+        'asunto',
+        'descripcion'
+    ];
+
+    //Enum -> select document type
+    protected $casts = [
+        'document_type' => TypeEnum::class,
+    ];
 }
---
+++ app/TypeEnum.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+enum TypeEnum:string
+{
+    case Crimson = 'Crimson';
+    case Amber = 'Amber';
+    case Velvet = 'Velvet';
+}
