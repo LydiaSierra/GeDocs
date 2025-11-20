658af0c4fc5349000ca617de293e7f22fdcefa05
Joalregra <johanalexis808@gmail.com>
2025-11-20 12:06:45 -0500

    Add component NotificationMenu

--- resources/js/Layouts/NotificationsLayout.jsx
+++ resources/js/Layouts/NotificationsLayout.jsx
@@ -3,14 +3,15 @@ import Sidebar from "@/Components/Sidebar/Sidebar.jsx";
 import React, {useContext} from "react";
 import {NotificationsContext} from "@/context/Notifications/NotificationsContext.jsx";
 import NotificationSidebar from "@/Components/Notifications/NotificationSidebar.jsx";
+import NotficationMenu from "../Components/Notifications/NotficationMenu"
 
 const NotificationsLayout = ({children}) => {
     return (
         <>
-            <div className='bg-gray-100'>
+            <div className='bg-gray-100 flex flex-col h-screen w-screen justify-center'>
                 <Header/>
-                <div className='flex h-screen'>
-                    <Sidebar/>
+                <div className='flex h-full pl-3 pb-3 pt-12 rounded-lg'>
+                    <NotficationMenu/>
 
                     <div className='pt-16 pb-2 w-full h-full flex'>
 
