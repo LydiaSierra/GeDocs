cde2dfe31eddd99a7806d6f799c4c5a7a17dc40e
Juan Manuel Velez <jumavxp@hotmail.com>
2025-11-21 00:33:13 -0500

    creating crud methods for CardController

--- app/Http/Controllers/CardController.php
+++ app/Http/Controllers/CardController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Card;
 use Illuminate\Http\Request;
+
 
 class CardController extends Controller
 {
@@ -12,7 +14,12 @@ class CardController extends Controller
      */
     public function index()
     {
-        //
+        //Mostrar la lista de fichas disponibles? 
+        $cards = Card::get();
+        if(!$cards){
+            $cards = 'No hay fichas registradas';
+        }
+        return []; //Retornar la vista
     }
 
     /**
@@ -20,7 +27,9 @@ class CardController extends Controller
      */
     public function create()
     {
-        //
+
+        return [];//Retornar la vista para crear fichas.
+
     }
 
     /**
@@ -28,7 +37,17 @@ class CardController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
+        $cardName = $request->input('card_name');
+        $checkIfExist = Card::where('name',$cardName);
+        //Revisar si la ficha ya existe antes de crearla
+        if($checkIfExist){
+            return response()->json(['success'=>false,'message'=>'La ficha ya está registrada'],400);
+        }else{
+        Card::create($cardName);
+            return response()->json(['success'=>true,'messake'=>'La ficha '.$cardName.' fue creada correctamente'],201);
+        }
+     
     }
 
     /**
@@ -37,6 +56,8 @@ class CardController extends Controller
     public function show(string $id)
     {
         //
+        $card = Card::find($id);
+        return []; //Retornar vista para el id
     }
 
     /**
@@ -53,6 +74,16 @@ class CardController extends Controller
     public function update(Request $request, string $id)
     {
         //
+        $card = Card::find($id);
+        if($card->name != $request->input('card_name'))
+        {
+            $card->name = $request->input('card_name');
+            $card->save();
+        }else
+        {
+            //Mensaje de error nuevo nombre es igual al nombre registrado ya
+            return;
+        }
     }
 
     /**
@@ -61,5 +92,6 @@ class CardController extends Controller
     public function destroy(string $id)
     {
         //
+        Card::delete($id);
     }
 }
