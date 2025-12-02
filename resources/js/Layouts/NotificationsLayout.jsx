0f458cece6f04b3946f7c499dfb3866dce9dd370
Joalregra <johanalexis808@gmail.com>
2025-12-01 22:39:01 -0500

    Update the settings sidebar

--- resources/js/Layouts/NotificationsLayout.jsx
+++ resources/js/Layouts/NotificationsLayout.jsx
@@ -3,7 +3,7 @@ import Sidebar from "@/Components/Sidebar/Sidebar.jsx";
 import React, {useContext} from "react";
 import {NotificationsContext} from "@/context/Notifications/NotificationsContext.jsx";
 import NotificationSidebar from "@/Components/Notifications/NotificationSidebar.jsx";
-import NotficationMenu from "../Components/Notifications/NotficationMenu"
+import SettingsBar from "../Components/SettingsBar/SettingsBar"
 
 const NotificationsLayout = ({children}) => {
     return (
@@ -11,7 +11,7 @@ const NotificationsLayout = ({children}) => {
             <div className='bg-gray-100 flex flex-col h-screen w-screen justify-center'>
                 <Header/>
                 <div className='flex h-full pl-3 pb-3 pt-12 rounded-lg'>
-                    <NotficationMenu/>
+                    <SettingsBar/>
 
                     <div className='pt-1.5 pb-2 w-full h-full '>
                         <div className={"p-2 w-full h-full flex flex-row gap-5"}>
--- resources/js/Layouts/UsersLayout.jsx
+++ resources/js/Layouts/UsersLayout.jsx
@@ -1,9 +1,6 @@
 import Header from "@/Components/Header/Header.jsx";
-import Sidebar from "@/Components/Sidebar/Sidebar.jsx";
 import React, {useContext} from "react";
-import {NotificationsContext} from "@/context/Notifications/NotificationsContext.jsx";
-import NotificationSidebar from "@/Components/Notifications/NotificationSidebar.jsx";
-import NotficationMenu from "../Components/Notifications/NotficationMenu"
+import SettingsBar from "../Components/SettingsBar/SettingsBar"
 
 const UsersLayout = ({children}) => {
     return (
@@ -11,7 +8,7 @@ const UsersLayout = ({children}) => {
             <div className='bg-gray-100 flex flex-col h-screen w-screen justify-center'>
                 <Header/>
                 <div className='flex h-full pl-3 pb-3 pt-12 rounded-lg gap-10 '>
-                    <NotficationMenu/>
+                    <SettingsBar/>
 
                     <div className='pt-3 pb-2 w-full h-full '>
                         <div className={"p-2 w-full h-full flex flex-col gap-5 pr-10"}>
