0d75f2ae840f001bdd5960b92af6a7455ebcdf84
Sebastian18131 <sebastian_castellang@soy.sena.edu.co>
2025-12-12 11:27:01 -0500

    Crud of the Sheets and implementation of functionality in the profile

--- resources/js/Pages/Profile/Edit.jsx
+++ resources/js/Pages/Profile/Edit.jsx
@@ -1,53 +1,30 @@
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
-import DeleteUserForm from './Partials/DeleteUserForm';
-import UpdatePasswordForm from './Partials/UpdatePasswordForm';
-import UpdateProfileInformationForm from './Partials/UpdateProfileInformationForm';
-import { DashboardLayout } from '@/Layouts/DashboardLayout';
-import ProfileLayout from '@/Layouts/ProfileLayout.jsx';
-=======
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head } from "@inertiajs/react";
 import DeleteUserForm from "./Partials/DeleteUserForm";
 import UpdatePasswordForm from "./Partials/UpdatePasswordForm";
 import UpdateProfileInformationForm from "./Partials/UpdateProfileInformationForm";
 import { DashboardLayout } from "@/Layouts/DashboardLayout";
-import ProfileLayaout from "@/Layouts/ProfileLayaout";
+import ProfileLayout from "@/Layouts/ProfileLayout.jsx";
+
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Head } from "@inertiajs/react";
+import DeleteUserForm from "./Partials/DeleteUserForm";
+import UpdatePasswordForm from "./Partials/UpdatePasswordForm";
+import UpdateProfileInformationForm from "./Partials/UpdateProfileInformationForm";
+import { DashboardLayout } from "@/Layouts/DashboardLayout";
+
 import { useState } from "react";
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 
 export default function Edit({ mustVerifyEmail, status }) {
     const [openObject, setOpenObject] = useState(false);
     const [openObject1, setOpenObject1] = useState(false);
     return (
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-        <ProfileLayout>
-
-
-=======
-        <ProfileLayaout setOpenObject={setOpenObject} openObject={openObject} openObject1={openObject1} setOpenObject1={setOpenObject1}> 
->>>>>>> Stashed changes
-=======
-        <ProfileLayaout setOpenObject={setOpenObject} openObject={openObject} openObject1={openObject1} setOpenObject1={setOpenObject1}> 
->>>>>>> Stashed changes
-=======
-        <ProfileLayaout setOpenObject={setOpenObject} openObject={openObject} openObject1={openObject1} setOpenObject1={setOpenObject1}> 
->>>>>>> Stashed changes
+        <ProfileLayout
+            setOpenObject={setOpenObject}
+            openObject={openObject}
+            openObject1={openObject1}
+            setOpenObject1={setOpenObject1}
+        >
             <div className="flex-1 h-[calc(100vh-4rem)] overflow-y-auto overflow-x-hidden py-4 px-4">
                 <div className="mx-auto max-w-4xl w-full space-y-4">
                     <div className="bg-white p-4 shadow-sm rounded-lg">
@@ -56,19 +33,10 @@ export default function Edit({ mustVerifyEmail, status }) {
                             status={status}
                             className="max-w-xl"
                         />
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
                     </div>
 
-                   <div className="hidden md:block bg-white p-4 shadow-sm rounded-lg">
+                    <div className="hidden md:block bg-white p-4 shadow-sm rounded-lg">
                         <UpdatePasswordForm className="max-w-xl" />
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
                     </div>
 
                     {openObject && (
@@ -76,11 +44,11 @@ export default function Edit({ mustVerifyEmail, status }) {
                             <UpdatePasswordForm className="max-w-xl" />
                         </div>
                     )}
-                    {openObject1 &&(
-                    <div className="hidden md:block bg-white p-4 shadow-sm rounded-lg">
-                        <DeleteUserForm className="max-w-xl" />
-                    </div>
-                   )}   
+                    {openObject1 && (
+                        <div className="hidden md:block bg-white p-4 shadow-sm rounded-lg">
+                            <DeleteUserForm className="max-w-xl" />
+                        </div>
+                    )}
                 </div>
             </div>
         </ProfileLayout>
