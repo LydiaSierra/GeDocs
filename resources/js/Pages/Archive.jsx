f0f44be60f8142ee90cd627dd20f8a153d4efd69
Lydia <lydia.sierra.ibata@gmail.com>
2026-03-04 21:31:05 -0500

    fix unused import

--- resources/js/Pages/Archive.jsx
+++ resources/js/Pages/Archive.jsx
@@ -1,8 +1,5 @@
-import Sidebar from "@/components/Sidebar/Sidebar";
-import { FunnelIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import ArchiveTable from "@/components/ArchiveTable/ArchiveTable";
 import { InputSearch } from "@/components/ArchiveExplorer/InputSearch";
-import Header from "@/Components/Header/Header";
 import { DashboardLayout } from "@/Layouts/DashboardLayout";
 
 export default function Archive() {
