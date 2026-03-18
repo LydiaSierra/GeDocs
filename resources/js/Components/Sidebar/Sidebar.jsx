a5c9244ad73c41fe9b519aaad7285477300f8367
Sebastian18131 <sebastian_castellang@soy.sena.edu.co>
2026-03-18 10:00:03 -0500

    Change ArchiveIcon to InboxIconEnv

--- resources/js/Components/Sidebar/Sidebar.jsx
+++ resources/js/Components/Sidebar/Sidebar.jsx
@@ -1,11 +1,16 @@
 import {
-  ArchiveBoxIcon,
   FolderIcon,
   InboxIcon,
   PlusCircleIcon
 } from "@heroicons/react/24/solid";
 import NavLink from "../NavLink";
 import { Link, usePage } from "@inertiajs/react";
+
+const InboxIconEnv = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M9 3.75H6.912a2.25 2.25 0 0 0-2.15 1.588L2.35 13.177a2.25 2.25 0 0 0-.1.661V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 0 0-2.15-1.588H15M2.25 13.5h3.86a2.25 2.25 0 0 1 2.012 1.244l.256.512a2.25 2.25 0 0 0 2.013 1.244h3.218a2.25 2.25 0 0 0 2.013-1.244l.256-.512a2.25 2.25 0 0 1 2.013-1.244h3.859M12 3v8.25m0 0-3-3m3 3 3-3" />
+  </svg>
+);
 
 export default function Sidebar() {
     const { props } = usePage();
@@ -15,7 +20,7 @@ export default function Sidebar() {
 
     const links = [
         { id: "inbox", href: "inbox", icon: InboxIcon},
-        { id: "archive", href: "archive", icon: ArchiveBoxIcon },
+        { id: "archive", href: "archive", icon: InboxIconEnv },
         { id: "explorer", href: "explorer", icon: FolderIcon },
     ];
 
